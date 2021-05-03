b5bc0ed22a26a566458087a24476691f9837a9a1
dnguy24 <dnguy24@u.rochester.edu>
2021-05-03 20:14:38 +0700

    update main program
    program reads array of integers from in-line arguments  and prints out phonetic equivalent  to stdout

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
-``
+package main
+
+import (
+    "fmt"
+    "os"
+	"strconv"
+	"strings"
+)
+
+var digits = []string{"Zero", "One", "Two", "Three", "Four", "Five",
+					  "Six", "Seven", "Eight", "Nine",}
+
+func main() {
+	// Get all the input arguments
+	args := os.Args[1:]
+	
+	var output []string
+	// Loop through each argument
+	for i := 0; i < len(args); i+=1 {
+		arg := args[i]
+		str := ""
+		// Loop through each digit
+		for j := 0; j < len(arg); j+=1 {
+			// Convert to string
+			number, _ := strconv.Atoi(string(arg[j]))
+			// Add each phonetic of a digit
+			str += digits[number]
+		}
+		// Print out the number
+		output = append(output, str)
+	}
+	fmt.Println(strings.Join(output, ","))
+}
